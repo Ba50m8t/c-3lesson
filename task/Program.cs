4203251613c44d7d21400d0e76806539583c46c6
Ba50m8t <aleksejbaf@gmail.com>
2023-04-25 11:15:52 +0300

    задача 23 решена

---
+++ task/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел от 1 до {N}:");
+for (int i = 1; i <= n; i++) 
+    {
+    Console.WriteLine("{0}^3 = {1}", i, i*i*i);
+    }
