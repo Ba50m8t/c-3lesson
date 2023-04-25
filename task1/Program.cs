218acd8ba7a29f1789ea29d817a775b3821ccdc7
Ba50m8t <aleksejbaf@gmail.com>
2023-04-25 10:43:11 +0300

    задача 19 решена

---
+++ task1/Program.cs
@@ -0,0 +1,50 @@
+﻿while (true)
+{
+    int userNumber = GetUserNumber("Введите пятизначное число: ");
+    int firstNumber = GetNumberByIndex(userNumber, 1);
+    int lastNumber = GetNumberByIndex(userNumber, 5);
+    int secondNumber = GetNumberByIndex(userNumber, 2);
+    int fourthNumber = GetNumberByIndex(userNumber, 4);
+    if (userNumber >= 10000 && userNumber < 100000)
+    {
+        if (firstNumber == lastNumber && secondNumber == fourthNumber)
+            Console.WriteLine($"{userNumber} является палиндромом");
+        else
+            Console.WriteLine($"{userNumber} не является палиндромом");
+        break;
+    }
+    else
+        Console.WriteLine($"ОШИБКА! {userNumber} НЕ ЯВЛЯЕТСЯ ПЯТИЗНАЧНЫМ");
+}
+
+static int GetUserNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userInput;
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕКОРРЕКТНЫЕ ДАННЫЕ");
+    }
+}
+static int GetNumberByIndex(int number, int index)
+{
+    int numberByIndex;
+    if (index == 1)
+    {
+        numberByIndex = number / 10000; 
+        return numberByIndex;
+    }
+    else
+    {
+        while (number >= Math.Pow(10, index))
+        {
+            number /= 10;
+        }
+        numberByIndex = number % 10;
+        return numberByIndex;
+    }
+}
